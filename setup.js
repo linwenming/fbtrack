41869537add4e127a9d13a3329662f60b4965b9e
linwenming <511709661@qq.com>
2022-06-12 21:46:34 +0800

    Update setup.js

---
+++ setup.js
@@ -0,0 +1,8 @@
+<!-- AdsupTrack Tracking Code -->
+<script>!function(e,t,n,s,a){(a=t.createElement(n)).async=!0,a.src="https://cdn.jsdelivr.net/gh/linwenming/fbtrack@v1.0.1/event.js",(t=t.getElementsByTagName(n)[0]).parentNode.insertBefore(a,t),e[s]=e[s]||function(){(e[s].q=e[s].q||[]).push(arguments)}}(window,document,"script","AdsupTrack");</script>
+<!-- End AdsupTrack Tracking Code -->
+
+<script>
+  // trigger an Purchase click event
+  track('code', 'Purchase');
+</script>
